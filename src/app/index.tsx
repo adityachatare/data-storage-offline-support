53f65be7163eeb9ce066c4741be155dea36fd584
Aditya Chatare <aditya.chatare@brightblu.com>
2026-05-27 14:43:01 +0530

    a

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,77 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { Button } from "expo-router/build/react-navigation";
+// import { createAsyncStorage } from "@react-native-async-storage/async-storage";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Index() {
+  const [data, setData] = useState("");
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+  // create a storage instance
+  // const storage = createAsyncStorage("appDB");
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+  const obj = {
+    name: "Aditya",
+    age: 30,
+    isDeveloper: true,
+  };
+  //SetItem
+  const saveData = async () => {
+    await AsyncStorage.setItem("user", JSON.stringify(obj));
+  };
+
+  //getItem
+  const getData = async () => {
+    const value = await AsyncStorage.getItem("user");
+    setData(value!);
+  };
+
+  //removeItem
+  const removeData = async () => {
+    await AsyncStorage.removeItem("user");
+  };
+
+  //clearStorage
+  const clearStorage = async () => {
+    await AsyncStorage.clear();
+    setData("");
+  };
+  const getKeys = async () => {
+    const keys = await AsyncStorage.getAllKeys();
+    console.log(keys);
+  };
+
+  // const saveMultiple = async ()=>{
+  //   await AsyncStorage.multiSet([
+  //     ['name','Code Snippet'],
+  //     ['role','Developer']
+  //   ])
+  // }
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", padding: 20, gap: 12 }}
+    >
+      {/* <Button title="Create Storage" onPress={storage} /> */}
 
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+      <Button title="Save Data" onPress={saveData} />
+      <Button title="Get Data" onPress={getData} />
+      <Button title="Remove Data" onPress={removeData} />
+      <Button title="Clear Storage" onPress={clearStorage} />
+      <Button title="Get All Keys" onPress={getKeys} />
+      {/* <Button title="Multi Set" onPress={saveData}/> */}
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+      <View style={styles.container}>
+        <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      </View>
+      <Text>{data}</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
